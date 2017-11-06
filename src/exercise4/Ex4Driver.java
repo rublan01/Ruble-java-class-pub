bc9f40b25bbdab5efca6dac9586c7ba574d3438d
anna <anna@anna-virtual-machine>
2017-11-06 00:30:47 -0600

    main file done

--- src/exercise4/Ex4Driver.java
+++ src/exercise4/Ex4Driver.java
@@ -2,6 +2,7 @@ package exercise4;
 
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.HashMap;
 
 /**
  * @author rublan01
@@ -14,6 +15,7 @@ public class Ex4Driver {
         task4("Animal");
         task5(1, 2);
         task6("Word", 3);
+        
         Random rnd = new Random();
         rnd.setSeed(252);
         int[] arr = new int[10];
@@ -21,6 +23,24 @@ public class Ex4Driver {
             arr[i] = rnd.nextInt(100);
         }
         task7(arr);
+        task8(arr, 8);
+       
+        ArrayList<String> strLst = new ArrayList();
+        strLst.add("Item 1");
+        strLst.add("Item 2");
+        strLst.add("Item 3");
+        strLst.add("Item 4");
+   
+        task9(strLst);
+        
+        HashMap<Integer, String> hashMap = new HashMap<Integer, String>();
+        hashMap.put(0, "zero");
+        hashMap.put(1, "one");
+        hashMap.put(2, "two");
+        hashMap.put(3, "three");
+        hashMap.put(4, "four");
+        
+        task10(hashMap);
     }
   /*
   Define a function that takes 1 integer as a parameter and returns an integer. 
@@ -96,16 +116,33 @@ Define a function that takes an array of integers and an integer as parameters a
 public Boolean task8(Integer[], Integer);
     */
     public static boolean task8(int[] arr, int n) {
-        int first = 0;
-        int plus;
-        first = arr[0];
-        plus = first + n;
-        return plus;
+        boolean ans = false;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == n) {
+                ans = true;
+            } else {
+                ans = false;
+            }
+        } return ans; 
     }
     /*
-Define a function that takes an ArrayList of Strings as a parameter and returns an integer. Write 2 unit tests to verify the function validity.
+Define a function that takes an ArrayList of Strings as a parameter and returns an integer. 
+    Write 2 unit tests to verify the function validity.
 public Integer task9(ArrayList<String>);
-Define a function that takes a HashMap (exact details are up to you) as a parameter and returns an integer. Write 2 unit tests to verify the function validity.
+    */
+    public static int task9(ArrayList<String> strlist) {
+        int count = 0;
+        count = strlist.size();
+        return count;
+    }
+    /*
+Define a function that takes a HashMap (exact details are up to you) as a parameter
+    and returns an integer. Write 2 unit tests to verify the function validity.
 public Integer task10(HashMap<>);
   */
+    public static int task10(HashMap map) {
+        int size = 0;
+        size = map.size();
+        return size;
+    }
 }
