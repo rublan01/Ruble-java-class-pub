c51b39e505367d02825ac7c7d6b7d37ec781e46a
anna <anna@anna-virtual-machine>
2017-11-08 21:49:09 -0600

    adding company class

--- src/exercise5/Company.java
+++ src/exercise5/Company.java
@@ -2,9 +2,35 @@ package exercise5;
 
 /**
  * Class Company
- * Task 4: Create a class Company with the following private data members: String name, double stockPrice. Implement a setter method for the stockPrice.
- * @author yasiro01
+ * Task 4: Create a class Company with the following private data members: 
+ * String name, double stockPrice. Implement a setter method for the stockPrice.
+ * @author rublan01
  */
 public class Company {
+    private final String name;
+    private double stockPrice;
+    
+    public Company(String name, double stockPrice) {
+        this.name = name;
+        this.stockPrice = stockPrice;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public double getStockPrice() {
+        return stockPrice;
+    }
+    
+    public double setStockPrice(double newStockPrice) {
+        stockPrice = newStockPrice;
+        return newStockPrice;
+    }
+    
+    @Override
+    public String toString() {
+        return String.format("%s (%f)", name, stockPrice);
+    }
   
 }
