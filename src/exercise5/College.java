cfeaaf910c4c00163cc64f4d7e979131f6764236
anna <anna@anna-virtual-machine>
2017-11-08 21:57:37 -0600

    adding college class

--- src/exercise5/College.java
+++ src/exercise5/College.java
@@ -2,9 +2,36 @@ package exercise5;
 
 /**
  * Class College
- * Task 5: Create a class College with the following private data members: String name, int rank. Implement setter method for the rank.
- * @author yasiro01
+ * Task 5: Create a class College with the following private data members: 
+ *      String name, int rank. 
+ * Implement setter method for the rank.
+ * @author rublan01
  */
 public class College {
+  private final String name;
+  private int rank;
+  
+  public College(String name, int rank) {
+      this.name = name; 
+      this.rank = rank;
+  }
+  
+  public String getName() {
+      return name;
+  }
+  
+  public int getRank() {
+      return rank;
+  }
+  
+  public int setRank(int newRank) {
+      rank = newRank;
+      return rank;
+  }
+  
+  @Override
+  public String toString() {
+      return String.format("%s (%d)", name, rank);
+  }
   
 }
