129e5aab359f82964feb2fca6eb5e18882228193
anna <anna@anna-virtual-machine>
2017-11-10 00:09:52 -0600

    last changes

--- src/exercise5/Roster.java
+++ src/exercise5/Roster.java
@@ -10,10 +10,21 @@ import java.util.regex.Pattern;
 
 /**
  * Class Roster
- * Task 10: Create a class Roster that contains a private member ArrayList<Student> studentList (reuse class Student from task 9). Its constructor must take the name of a file as a parameter, read data from the file (sample roster.csv is provided, another file of a similar structure may be used for grading), create an object of type Student based on the content of a line, and add that object to the ArrayList studentList.
+ * Task 10: Create a class Roster that contains a private member ArrayList<Student> studentList 
+ * (reuse class Student from task 9). Its constructor must take the name of a file as a parameter,
+ * read data from the file (sample roster.csv is provided, another file of a similar structure
+ * may be used for grading), create an object of type Student based on the content of a line, 
+ * and add that object to the ArrayList studentList.
  * Implement method printRoster that prints all the elements of the studentList.
- * @author yasiro01
+ * @author rublan01
  */
 public class Roster {
+  private final ArrayList<Student> studentList;
   
+  public Roster(String filename) throws FileNotFoundException, IOException {
+      studentList = new ArrayList();
+      BufferedReader inputFile = new BufferedReader(new FileReader(filename));
+      String line;
+      Pattern p = Pattern.compile("(\\d+)\\s+(\\w+(\\s")
+  }
 }
