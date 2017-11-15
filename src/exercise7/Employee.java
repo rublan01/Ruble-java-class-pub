05c6c3c779c080a6fcbcd2d97e74a4032353a34b
anna <anna@anna-virtual-machine>
2017-11-15 17:58:43 -0600

    adding class Employee

--- src/exercise7/Employee.java
+++ src/exercise7/Employee.java
@@ -5,4 +5,39 @@ package exercise7;
  */
 public class Employee {
     
+    protected String name;
+    protected String ssn ="000-00-0000";
+    protected double netPay;
+
+    public Employee(String name, String ssn) {
+        this.name = name;
+        this.ssn = ssn;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+    public double getNetPay() {
+        return netPay;
+    }
+
+    public void setNetPay(double netPay) {
+        this.netPay = netPay;
+    }
+    
+    
+    
 }
