2265375b3c8d81059bd6443551d91d4a42abc0a2
anna <anna@anna-virtual-machine>
2017-11-15 21:10:54 -0600

    adding implementations of Hourly and Salaried Employees

--- src/exercise7/HourlyEmployee.java
+++ src/exercise7/HourlyEmployee.java
@@ -3,6 +3,36 @@ package exercise7;
 /**
  * @author anna
  */
-public class HourlyEmployee {
+public class HourlyEmployee extends Employee {
+    
+    private double wageRate;
+    private double hours;
+
+    public HourlyEmployee(double wageRate, double hours, String name, String ssn) {
+        super(name, ssn);
+        this.wageRate = wageRate;
+        this.hours = hours;
+    }
+
+    public double getWageRate() {
+        return wageRate;
+    }
+
+    public void setWageRate(double wageRate) {
+        this.wageRate = wageRate;
+    }
+
+    public double getHours() {
+        return hours;
+    }
+
+    public void setHours(double hours) {
+        this.hours = hours;
+    }
+    
+    public void printWeeklyCheck() {
+        double weekSalary = 0;
+        weekSalary = (hours * wageRate) / 52;
+    }
     
 }
--- src/exercise7/SalariedEmployee.java
+++ src/exercise7/SalariedEmployee.java
@@ -3,6 +3,26 @@ package exercise7;
 /**
  * @author anna
  */
-public class SalariedEmployee {
+public class SalariedEmployee extends Employee {
+    
+    private double salary;
+
+    public SalariedEmployee(double salary, String name, String ssn) {
+        super(name, ssn);
+        this.salary = salary;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+    
+    public void printWeeklyCheck() {
+        double weekSalary = 0;
+        weekSalary = salary / 52;
+    }    
     
 }
