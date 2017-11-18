67bc0be5a37c9dd7d9c603e504f30b577cb86324
anna <anna@anna-virtual-machine>
2017-11-18 14:45:38 -0600

    adding class DigitalMovie

--- src/exercise8/DigitalMovie.java
+++ src/exercise8/DigitalMovie.java
@@ -5,11 +5,30 @@ import java.util.ArrayList;
 /**
  * Class DigitalMovie
  * MediaLIbrary code 2
- * @author yasiro01
+ * @author rublan01
  */
 public class DigitalMovie extends Movie {
   private String service;
 
+    public DigitalMovie(String service, String director, int releaseYear, long id, String title, int quantity) {
+        super(director, releaseYear, id, title, quantity);
+        this.service = service;
+    }
+    
+    @Override
+    public void checkout() {
+        
+    }
+    
+    @Override
+    public void checkin() {
+        
+    }
+    
+    public String getInfo() {
+        return (title + "(" + releaseYear + ") by " + director + "is avaliable via " + service);
+    }
+  
   /**
    * Get the value of service
    * @return the value of service
