8f0c250c143512c2d55749194fdcbf4804c11de3
anna <anna@anna-virtual-machine>
2017-11-18 14:19:04 -0600

    adding class Item

--- src/exercise8/Item.java
+++ src/exercise8/Item.java
@@ -2,12 +2,34 @@ package exercise8;
 
 /**
  * Class Item
- * @author yasiro01
+ * @author rublan01
  */
 public abstract class Item {
   protected final long id;
   protected final String title;
   protected int quantity;
+
+  public Item(long id, String title, int quantity) {
+    this.id = id;
+    this.title = title;
+    this.quantity = quantity;
+   }
+  
+    public long getID() {
+        return id;
+    }
+    
+    public String getTitle() {
+        return title;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 
   /**
    * Get the item availability
@@ -21,19 +43,32 @@ public abstract class Item {
    * Check out an item (decrease quantity by 1, if possible
    */
   public void checkout() {
-    throw new UnsupportedOperationException();
+    if (quantity > 0) {
+        quantity = quantity - 1;
+    }
+    else {
+        System.out.println("Cannot checkout");
+    }
   }
 
   /**
    * Check in an item
    */
   public void checkin() {
-      throw new UnsupportedOperationException();
+      quantity = quantity + 1;
   }
   
   @Override
   public String toString() {
-    throw new UnsupportedOperationException();
+      if (quantity > 1) {
+          return ("We have" + quantity +  "copies of " + title);
+      }
+      else if (quantity == 1) {
+          return ("We have a copy of " + title);
+      }
+      else {
+          return ("No copies avaliable");
+      }
   }
 
   /**
