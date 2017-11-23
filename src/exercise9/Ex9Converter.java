a92c6a84e065e635560b9fe4dcb38abee6685ad7
anna <anna@anna-virtual-machine>
2017-11-22 18:41:34 -0600

    adding more

--- src/exercise9/Ex9Converter.java
+++ src/exercise9/Ex9Converter.java
@@ -8,32 +8,32 @@ public class Ex9Converter {
     
     
   public Double c2f(Double valueFrom) {
-      double f = (valueFrom * 9/5) + 32;
-      return f;
+      double temp = (valueFrom * 9/5) + 32;
+      return temp;
   }
 
   public Double c2k(Double valueFrom) {
-      double k = (valueFrom + 273.15);
-      return k;
+      double temp = (valueFrom + 273.15);
+      return temp;
   }
 
   public Double f2c(Double valueFrom) {
-      double c = (valueFrom - 32) * 5/9;
-      return c;
+      double temp = (valueFrom - 32) * 5/9;
+      return temp;
   }
 
   public Double f2k(Double valueFrom) {
-      double k = ((valueFrom - 32) * 5/9 + 273.15);
-      return k;
+      double temp = ((valueFrom - 32) * 5/9 + 273.15);
+      return temp;
   }
 
   public Double k2c(Double valueFrom) {
-      double c = (valueFrom - 273.15);
-      return c;
+      double temp = (valueFrom - 273.15);
+      return temp;
   }
 
   public Double k2f(Double valueFrom) {
-      double f = ((valueFrom - 273.15) * 9/5 + 32);
-      return f;
+      double temp = ((valueFrom - 273.15) * 9/5 + 32);
+      return temp;
   }
 }
